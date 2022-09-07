764b5ee8125e24d710f69a7c32bce98ef4897f5f
valpalsgraf <vpalsgraf@loopstudio.dev>
2022-09-07 10:51:21 -0300

    added messages and rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :rooms
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms do
+    resources :messages
+  end
 end
